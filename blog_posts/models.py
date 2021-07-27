182cf02d5ec690250889110461c51f5f560f9d30
ins4w <amejia.foo@gmail.com>
2021-07-27 16:55:23 -0500

    Configured/Migrated: Custom routing and migrated models to PostgreSQL

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+# In order to update database models you need to migrate the changes
+
 # Create your models here.
+class Post(models.Model): # Inherit django database models features
+    title = models.CharField(max_Length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
