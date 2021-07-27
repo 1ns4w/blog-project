b21e78feb5e55912c868682b376afe3b018416f9
ins4w <amejia.foo@gmail.com>
2021-07-27 17:02:38 -0500

    Registered posts model to Django admin panel

--- blog_posts/admin.py
+++ blog_posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# To migrate the models to the admin panel we need to set up admin.py
+from .models import Post
 
 # Register your models here.
+admin.site.register(Post)
